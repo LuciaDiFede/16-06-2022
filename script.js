6b2f6a5c360b164fd4216eaf9eef8f43c9b0b2da
LuciaDiFede <difedelucia01@gmail.com>
2022-06-16 17:47:57 +0200

    chore: first init

---
+++ script.js
@@ -0,0 +1,63 @@
+// Calculator 
+
+// 1 chiedere all'utente di inserire un primo numero, l'operazione da effettuare e un secondo numero
+// 2 restituire all'utente il risultato tramite un alert()
+
+function operation() {
+  const firstNum = prompt('Inserisci il primo numero');
+  const operationChoice = prompt('Inserisci l\'operazione');
+  const secondNum = prompt('Inserisci il secondo numero');
+
+  const parsedFirstNum = parseInt(firstNum);
+  const parsedSecondNum = parseInt(secondNum);
+
+  if (firstNum.length === 0 || secondNum.length === 0) {
+    console.log("Non hai inserito un numero");
+  }
+  
+  switch (operationChoice) {
+    case "addizione":
+      console.log(
+        "Il risultato della tua operazione è:",
+        parsedFirstNum + parsedSecondNum
+      );
+      break;
+    case "sottrazione":
+      console.log(
+        "Il risultato della tua operazione è:",
+        parsedFirstNum - parsedSecondNum
+      );
+      break;
+    case "moltiplicazione":
+      console.log(
+        "Il risultato della tua operazione è:",
+        parsedFirstNum * parsedSecondNum
+      );
+      break;
+    case "divisione":
+      console.log(
+        "Il risultato della tua operazione è:",
+        parsedFirstNum / parsedSecondNum
+      );
+      break;
+    default:
+      console.log("Non hai selezionato alcuna operazione.");
+  }
+}
+
+console.log(operation());
+
+
+
+
+// // OBJECT 
+// const product = {
+//   name: "Gummy Bears",
+//   inStock: true,
+//   price: 1.99,
+//   flavors: ["grape", "apple", "cherry"],
+// };
+
+// console.log(product{}.price)
+
+// console.log(product{}.defineProperty('Gummy Bears'))
